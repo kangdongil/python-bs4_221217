82d2db5c9e9441292f1226610b6663497d759551
Noru <akang8150@naver.com>
2022-12-17 11:24:20 +0000

    Selenium: Scrapping Indeed Jobs depending on Total Pages

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import math
 from selenium import webdriver
 from config.options import options
 from bs4 import BeautifulSoup
+
+
+def get_total_pages(soup, limit):
+    try:
+        job_count_response = soup.find(
+            "div", class_="jobsearch-JobCountAndSortPane-jobCount"
+        ).text
+        job_total = int(job_count_response.split()[1][:-1].replace(",", ""))
+        page_total = math.ceil(job_total / limit)
+        if page_total > 10:
+            page_total = 10
+        print("Found", format(job_total, ","), "jobs", "\n")
+        return page_total
+    except AttributeError:
+        print("No Job Found", "\n")
+        return 0
 
 
 def parse_job_data(soup):
@@ -27,16 +44,24 @@ def parse_job_data(soup):
     return results
 
 
-def extract_indeed_jobs():
+def extract_indeed_jobs(keyword):
 
     base_url = "https://kr.indeed.com/jobs"
-    keyword = "python"
+    limit = 50
     results = []
 
     browser = webdriver.Chrome(options=options)
+    browser.get(f"{base_url}?q={keyword}")
+    print(f"Searching {keyword}...", "\n")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
 
-    for page in range(5):
-        request_url = f"{base_url}?q={keyword}&start={page*10}"
+    page_total = get_total_pages(soup, limit)
+
+    if page_total == 0:
+        return []
+
+    for page in range(page_total):
+        request_url = f"{base_url}?q={keyword}&limit={limit}&start={page*limit}"
         print(f"Requesting {request_url}")
 
         browser.get(request_url)
@@ -50,4 +75,7 @@ def extract_indeed_jobs():
     print(f"{keyword} 직업이 모두 {len(results)}개 발견되었습니다.", "\n")
 
 
-extract_indeed_jobs()
+extract_indeed_jobs("juypter")
+extract_indeed_jobs("nest")
+extract_indeed_jobs("react")
+extract_indeed_jobs("python")
