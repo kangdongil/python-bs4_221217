62cbfb42b3bb35feff2db54fc0e6e0d3e4b8bf99
Noru <akang8150@naver.com>
2022-12-17 10:47:12 +0000

    Fix ChromeDriver MemoryProblem & GitPod Direnv Reload

--- config/options.py
+++ config/options.py
@@ -9,6 +9,8 @@ user_agent_config = [
 
 options = Options()
 options.add_argument("headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("window-size=1920x1080")
 options.add_argument("disable-gpu")
 options.add_argument(f"user-agent={' '.join(user_agent_config)}")
