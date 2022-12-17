0e7942a45e5fb82c617fefb38a62778565b694cb
Noru <akang8150@naver.com>
2022-12-17 05:58:28 +0000

    Requests: Get & Parse Requested Data

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from selenium import webdriver
-from config.options import options
+import requests
+from bs4 import BeautifulSoup
 
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
 
-browser = webdriver.Chrome(options=options)
-
-# Selenium 사용하는 예시
-browser.get("https://google.com")
-browser.save_screenshot("success.png")
+response = requests.get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("해당 사이트를 Request하는데 실패하였습니다.")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    print(soup.find_all("section", class_="jobs"))
