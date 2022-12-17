46579edac42647acad479a3259a41bcda1c34237
Noru <akang8150@naver.com>
2022-12-17 10:53:26 +0000

    Selenium: Disable Recursive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from config.options import options
+from bs4 import BeautifulSoup
 
 
 base_url = "https://kr.indeed.com/jobs?q="
@@ -8,5 +9,10 @@ keyword = "python"
 browser = webdriver.Chrome(options=options)
 browser.get(f"{base_url}{keyword}")
 
-print(browser.page_source)
-browser.save_screenshot("success.png")
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all("li", recursive=False)
+for job in jobs:
+    print(job)
+    print("\n")
+    # mosaic-zone 제거하기
