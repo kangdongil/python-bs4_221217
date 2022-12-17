129d196af1e6101cb82c1ae74d18931cef54d225
Noru <akang8150@naver.com>
2022-12-17 10:53:55 +0000

    Selenium: Exclude Bad Data with None

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList")
 jobs = job_list.find_all("li", recursive=False)
 for job in jobs:
-    print(job)
-    print("\n")
-    # mosaic-zone 제거하기
+    zone = job.find("div", class_="mosaic-zone")
+    if zone is not None:
+        continue
