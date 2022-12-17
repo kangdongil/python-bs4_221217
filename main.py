bb6067e23685c70b3325fe97935c737764a28f64
Noru <akang8150@naver.com>
2022-12-17 10:55:51 +0000

    Selenium: Pick HTML Element with Select

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ keyword = "python"
 browser = webdriver.Chrome(options=options)
 browser.get(f"{base_url}{keyword}")
 
+results = []
 soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList")
 jobs = job_list.find_all("li", recursive=False)
@@ -16,3 +17,17 @@ for job in jobs:
     zone = job.find("div", class_="mosaic-zone")
     if zone is not None:
         continue
+    anchor = job.select_one("h2 a", class_="jobTitle")
+    position = job.find("h2", class_="jobTitle")
+    link = anchor["href"]
+    company = job.find("span", class_="companyName")
+    location = job.find("div", class_="companyLocation")
+    job_data = {
+        "link": f"https://kr.indeed.com/{link}",
+        "company": company.string,
+        "location": location.string,
+        "position": position.string,
+    }
+    results.append(job_data)
+for result in results:
+    print(result, "\n")
