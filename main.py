c51a2d340a2a5ac30612a1819e5f2c6e5f48445b
Noru <akang8150@naver.com>
2022-12-17 06:08:06 +0000

    Requests: Create Job Dictionaries

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ response = requests.get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("해당 사이트를 Request하는데 실패하였습니다.")
 else:
+    results = []
     soup = BeautifulSoup(response.text, "html.parser")
     jobs = soup.find_all("section", class_="jobs")
     for job in jobs:
@@ -17,5 +18,13 @@ else:
             position = post.find("span", class_="title")
             link = anchor["href"]
             company, _, region = anchor.find_all("span", class_="company")
-            print(position, link, company, region, sep="\n")
-            print("\n")
+            job_data = {
+                "link": f"https://weworkremotely.com{link}",
+                "company": company.string,
+                "region": region.string,
+                "position": position.string,
+            }
+            results.append(job_data)
+
+    for result in results:
+        print(result, sep="\n")
